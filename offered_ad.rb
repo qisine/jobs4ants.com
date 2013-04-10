d4933e15358138c046773ca3878a91384b2c83d6
sk <thanks@no.spam>
2013-04-09 23:13:50 -0400

    attrs indexed by string

--- offered_ad.rb
+++ offered_ad.rb
@@ -2,7 +2,7 @@ class OfferedAd < BaseAd
   def to_h
     h = {}
     [:id, :title, :body, :created_at, :updated_at].each do |e|
-      h[e] = (self.attributes[e] || "").to_s
+      h[e] = (self.attributes[e.to_s] || "").to_s
     end
     h
   end
