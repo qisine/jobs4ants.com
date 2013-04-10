da437af034e804115bb0208eed7ce6472853fc8d
sk <thanks@no.spam>
2013-04-10 13:11:08 -0400

    adding uuid for authorization

--- spec/factories/offered_ads.rb
+++ spec/factories/offered_ads.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :offered_ad, parent: :base_ad, class: 'OfferedAd' do
     pay { Random.new.rand(20..9999) }
+    uuid do
+      require 'securerandom'
+      SecureRandom.uuid
+    end
   end
 end
