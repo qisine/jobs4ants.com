b489f506a6c37d8f90dbacdb372cdd037c08925d
sk <thanks@no.spam>
2013-04-09 21:39:24 -0400

    id attr needs to be included

--- offered_ad.rb
+++ offered_ad.rb
@@ -1,7 +1,7 @@
 class OfferedAd < BaseAd
   def to_h
     h = {}
-    [:title, :body, :created_at, :updated_at].each do |e|
+    [:id, :title, :body, :created_at, :updated_at].each do |e|
       h[e] = (self.attributes[e] || "").to_s
     end
     h
