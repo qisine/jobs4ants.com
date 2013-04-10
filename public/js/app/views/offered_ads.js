530a8a58dbf2ed2c0ed9d23d3e5bc707d617114a
sk <thanks@no.spam>
2013-04-09 23:17:18 -0400

    el accepts string

--- public/js/app/views/offered_ads.js
+++ public/js/app/views/offered_ads.js
@@ -1,15 +1,15 @@
 window.App = window.App || { Collections: {}, Models: {}, Routers: {}, Views: {}, };
 App.Views.OfferedAds = Backbone.View.extend({
   type: "indexOfferedAds",
-  template: JST["offeredAds"],
-  el: $("#app"),
+  tmpl: JST["offeredAds"],
+  el: "#app",
 
   initialize: function() {
     this.collection = this.options.collection;
   },
 
   render: function() {
-    this.$el.html(this.template({collection: this.collection}));
+    this.$el.html(_.template(this.tmpl)({c: this.collection.models }));
     return this;
   },
 
