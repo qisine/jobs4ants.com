aa7b9da66b862dcd1025d7d184f2bbaaa2814c37
sk <thanks@no.spam>
2013-04-10 17:55:37 -0400

    the search bar

---
+++ app/assets/js/app/views/search_bar.js
@@ -0,0 +1,14 @@
+App.Views.SearchBar = Backbone.View.extend({
+  type: "searchBar",
+  tmpl: JST["js/app/templates/shared/search_bar"],
+  el: "#searchBar",
+
+  initialize: function() {
+  },
+
+  render: function() {
+  },
+
+  onClose: function() {
+  }
+});
