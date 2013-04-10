139f57ad48829cdf2cb0badac6dfa9d6793a5f14
sk <thanks@no.spam>
2013-04-10 11:58:45 -0400

    initial creation of models

---
+++ db/migrate/20130410153247_initial_create_models.rb
@@ -0,0 +1,28 @@
+class InitialCreateModels < ActiveRecord::Migration
+  def up
+    create_table "base_ads" do |t|
+      t.string   "type"
+      t.string   "title"
+      t.text     "body"
+      t.string   "uuid"
+      t.string   "work_location"
+      t.integer  "pay"
+      t.string   "link"
+      t.string   "source"
+      t.integer  "job_category_id"
+    end
+
+    add_index "base_ads", "job_category_id"
+    add_index "base_ads", "uuid"
+
+    create_table "job_categories" do |t|
+      t.string   "name"
+      t.string   "code"
+    end
+  end
+
+  def down
+    drop_table :base_ads
+    drop_table :job_categories
+  end
+end
