9ce6498d0a8cea793c723f3bc164b46fb56ace97
sk <thanks@no.spam>
2013-04-11 17:26:59 -0400

    fleshing out search bar

--- app/assets/js/app/views/search_bar.js
+++ app/assets/js/app/views/search_bar.js
@@ -1,9 +1,20 @@
 App.Views.SearchBar = Backbone.View.extend({
   type: "searchBar",
   tmpl: JST["js/app/templates/shared/search_bar"],
-  el: "#searchBar",
+  el: "#search-bar",
+  events: {
+    "click input[type=submit]": "search",
+  },
 
   initialize: function() {
+  },
+
+  search: function(ev) {
+    ev.preventDefault();
+    var kwds = this.$el.find("#general-kwds").val();
+
+    App.dispatcher.trigger("search:submit");
+    App.viewManager.add(new App.Views.OfferedAds({kwds: kwds}));
   },
 
   render: function() {
