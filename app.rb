fca757f67f745faba7b7ed9dd038f3224c664c9a
sk <thanks@no.spam>
2013-04-11 15:07:10 -0400

    pagination info will be reused

--- app.rb
+++ app.rb
@@ -17,16 +17,36 @@ get '/' do
 end
 
 get '/d/offered-ads' do
-  page = params[:page]
-  per_page = params[:perPage] || 25
-  per_page = per_page.to_i rescue 25
-  split = per_page / 2
-   
-  offered_ads = OfferedAd.paginate(page, per_page - split).all 
-  ex_ads = ExternalOfferedAd.paginate(page, split).all
+  page_info = get_pagination_info(params)
+
+  offered_ads = OfferedAd.paginate(page_info[:page], page_info[:per_page] - page_info[:split]).all 
+  ex_ads = ExternalOfferedAd.paginate(page_info[:page], page_info[:split]).all
   models = (offered_ads + ex_ads).sort { |a,b| a.created_at <=> b.created_at }.reverse.map(&:to_h)
-  json({ page: page, perPage: per_page, total: OfferedAd.count + ExternalOfferedAd.count, models: models })
+  json({  page: page_info[:page],
+          perPage: page_info[:per_page],
+          total: OfferedAd.count + ExternalOfferedAd.count,
+          models: models })
+end
+
+get '/d/offered-ads/search' do
+  kwd = params[:kwd] || ""
+  page_info = get_pagination_info(params)
+end
+
+get '/d/work-locations/search' do
+  kwd = params[:kwd] || ""
+
+  locs = WorkLocation.search(kwd).all
+  json(locations: locs)
 end
 
 helpers do
+  def get_pagination_info(p)
+    info = {}
+    info[:page] = p[:page]
+    per_page = p[:perPage] || 25
+    info[:per_page] = per_page.to_i rescue 25
+    info[:split] = per_page / 2
+    info
+  end
 end
