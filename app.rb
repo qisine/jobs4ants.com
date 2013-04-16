890ca837cf0aef70c3142487fb00b1748a503221
sk <thanks@no.spam>
2013-04-16 07:37:01 -0400

    no longer have this scope

--- app.rb
+++ app.rb
@@ -37,7 +37,7 @@ end
 
 get '/d/offered-ads/:id' do
   begin
-    json(OfferedAd.published.find(params[:id]).to_h)
+    json(OfferedAd.where(published: true).find(params[:id]).to_h)
   rescue ActiveRecord::RecordNotFound
     status 404
     json({error: "couldn't find requested record"})
