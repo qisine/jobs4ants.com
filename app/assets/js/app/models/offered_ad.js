e141ad05fdd5988fcdf3b93b48617c6ad6188402
sk <thanks@no.spam>
2013-04-16 04:50:47 -0400

    adding the caches, to save roundtrips/optimize

--- app/assets/js/app/models/offered_ad.js
+++ app/assets/js/app/models/offered_ad.js
@@ -1,7 +1,34 @@
 App.Models.OfferedAd = Backbone.Model.extend({
+  previewLength: 250,
+
+  constructor: function(attributes, options) {
+    if(attributes && attributes.id) {
+      App.Models.OfferedAd.cache.add(attributes);
+    }
+    Backbone.Model.apply(this, arguments);
+  },
+
   url: function() {
     base = 'd/offered-ads'
     if(this.isNew()) return base; 
     return base + '/' + this.id;
+  },
+
+  bodyPreview: function() {
+    if(!this._bodyPreviewString && this.body && this.body.length > this.previewLength) 
+      this._bodyPreviewString = this.body.substr(0, this.previewLength);
+
+    return this._bodyPreviewString;
   }
 });
+
+App.Models.OfferedAd.cache = new App.Cache;
+App.Models.OfferedAd.create = function(attrs) {
+  var ad;
+  if(attrs) {
+    ad = this.cache.get(attrs); 
+    if(ad) return ad;
+  }
+  ad = this.cache.add(new this(attrs));
+  return ad;
+}
