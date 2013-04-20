a1b93e322ebb95c42a6f498018d0d7275ea31136
sk <thanks@no.spam>
2013-04-20 05:29:28 -0400

    setting up CRUD routes

--- app.rb
+++ app.rb
@@ -11,8 +11,9 @@ require './app/models/base_ad.rb'
 Dir["./app/models/*.rb"].each { |f| require f }
 
 set :views, Proc.new { File.join(root, "app", "views") }
+enable :sessions
 
-get %r{^/(?!d/).*} do
+get %r{^/(?!(?:d/|offered-ads/\d+/(?:edit|delete))).*} do
   erb :index
 end
 
@@ -66,15 +67,18 @@ get '/d/job-categories' do
   json(JobCategory.all.map(&:to_h))
 end
 
+get %r{/offered-ads/(\d+)/(?:edit|delete)} do |id|
+  @ad = load_and_authorize(id, params[:uuid])
+  halt 401 if !@ad
+
+  session[:uuid] ||= {}
+  session[:uuid] = @ad.uuid
+  erb :index
+end
+
 post '/offered-ads' do
   begin
-    j = JSON.parse(request.env['rack.input'].read)
-    halt 400 if(!j || j.delete("url").try { |u| u.strip.size > 0 })
-
-    zip = j.delete("work_location_zip").try(:strip)
-    loc = WorkLocation.where("zip = ?", zip).first
-    loc = WorkLocation.where("zip = '0000'").first if !loc 
-    j['work_location_id'] = loc.id
+    j = parse_json(request.env['rack.input'].read)
 
     OfferedAd.create!(j)
     json({message: "ad successfully created"})
@@ -84,13 +88,54 @@ post '/offered-ads' do
   end
 end
 
-put '/offered-ad/:id' do
+put %r{/offered-ads/(\d+)} do |id|
+  require 'debugger'; debugger
+  begin
+    ad = load_and_authorize(id)
+    halt 401 if !ad
+
+    j = parse_json(request.env['rack.input'].read)
+    ad.update_attributes!(j)
+    json({message: "ad successfully updated"})
+  rescue JSON::ParserError
+    status 400
+    json({message: "invalid JSON format"})
+  end
+  
 end
 
-delete '/offered-ad/:id' do
+delete %r{/offered-ads/(\d+)} do |id|
+  ad = load_and_authorize(id)
+  halt 401 if !ad
+
+  if(ad.destroy)
+    json({message: "successfully deleted ad"})
+  else
+    status 500
+    json({message: "could not delete ad"})
+   end 
 end
 
 helpers do
+  def parse_json(json)
+    j = JSON.parse(json)
+    halt 400 if(!j || j.delete("url").try { |u| u.strip.size > 0 })
+
+    zip = j.delete("work_location_zip").try(:strip)
+    loc = WorkLocation.where("zip = ?", zip).first
+    loc = WorkLocation.where("zip = '0000'").first if !loc 
+    j['work_location_id'] = loc.id
+    j
+  end
+
+  def load_and_authorize(id, uuid=nil)
+    uuid ||= session[:uuid]
+    ad = OfferedAd.find id
+    uuid = params[:uuid].try :strip
+    return nil if ad.uuid != uuid
+    ad
+  end
+  
   def parse_params(p)
     info = {}
     info[:page] = p[:page] || 1
